fbd2246866ff52e490cda7a85f3283970c91fda6
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-01-09 16:29:12 +0100

    Simplifying the use of locking functions (all memomethods are locking by default if they can be)

--- src/memoclass/memoize.py
+++ src/memoclass/memoize.py
@@ -43,10 +43,12 @@ def make_decorator(decorator):
 
 class MemoFunc(object):
     """ Memoizes a free function """
-
     def __init__(self, func, cache_cls=dict, on_return=lambda x: x,
                  hasher=_to_hashable):
         """ Memoize a free function
+
+            The locks and clear_on_unlock parameters can usually be ignored by
+            users, they are used by the 'MemoMethod' class
 
             :param func: The function to memoize
             :param cache_cls: The type to use for caching, defaults to dict
@@ -62,6 +64,14 @@ class MemoFunc(object):
                 It will receive the callargs dictionary as an argument if func
                 satisifies inspect.isfunction, otherwise receives a two-tuple of
                 (args, kwargs), defaults to _to_hashable
+            :param locks:
+                If True, the function *must* be called with an object supplied
+                to the first argument that has a 'locked' method, taking one
+                argument that is a context manager (will usually be a MemoClass
+                object)
+            :param clear_on_unlock:
+                Parameter passed to the object in the first argument's locked()
+                method if locks is set and used
         """
         update_wrapper(self, func)
         self._wrapped_func = func
@@ -113,14 +123,37 @@ class MemoFunc(object):
         if key not in self._cache:
             self._cache[key] = self._wrapped_func(*args, **kwargs)
         return self._on_return(self._cache[key])
+
 memofunc = make_decorator(MemoFunc)
+
+class LockMemoFunc(MemoFunc):
+    """ Used to memoize a bound function on a lockable class
+
+        The bound function will lock its class before being called
+    """
+    def __init__(self, func, clear_on_unlock, **kwargs):
+        """ Initialise the bound function
+
+            :param clear_on_unlock: Parameter bound object's locked() method
+        """
+        super(LockMemoFunc, self).__init__(func, **kwargs)
+        self._clear_on_unlock = clear_on_unlock
+
+    def __call__(self, *args, **kwargs):
+        """ Call the function """
+        with self._wrapped_func.__self__.locked(self._clear_on_unlock):
+            return super(LockMemoFunc, self).__call__(*args, **kwargs)
 
 class MemoMethod(object):
     """ Memoizes a class' method """
 
     def __init__(self, func, cache_cls=dict, on_return=lambda x: x,
-                 hasher=_to_hashable, memofunc_cls=MemoFunc):
+                 hasher=_to_hashable, locks=True, clear_on_unlock=None):
         """ Memoize a bound method
+
+            As 'locks' defaults to True, if a class has a 'locked' function
+            (i.e. is a MemoClass) then a MemoFunc will default to locking the
+            class, but only if it can.
 
             :param func: The function to memoize
             :param cache_cls: The type to use for caching, defaults to dict
@@ -135,13 +168,19 @@ class MemoMethod(object):
                 The function that should be used to make the arguments hashable.
                 It will receive the callargs dictionary as an argument, defaults
                 to _to_hashable
+            :param locks:
+                If True, and the bound object has a 'locked' function, then will
+                use that as a context manager
+            :param clear_on_unlock:
+                If locks is used, the argument to the 'locked' function
         """
         self._wrapped_func = func
         self._cache_cls = cache_cls
         self._on_return = on_return
         self._hasher = hasher
         self._bound_methods = WeakKeyDictionary()
-        self._memofunc_cls = memofunc_cls
+        self._locks = locks
+        self._clear_on_unlock = clear_on_unlock
 
     def __get__(self, obj, objtype=None):
         if obj is None:
@@ -152,11 +191,17 @@ class MemoMethod(object):
             # Use python's internal function binding to make everything play
             # nice
             func = MethodType(self._wrapped_func, obj)
-            self._bound_methods[obj] = self._memofunc_cls(
-                    func=func,
-                    cache_cls = self._cache_cls,
-                    on_return = self._on_return,
-                    hasher = self._hasher)
+            kwargs = {
+                    'cache_cls' : self._cache_cls,
+                    'on_return' : self._on_return,
+                    'hasher'    : self._hasher}
+            if self._locks and hasattr(obj, 'locked') and callable(obj.locked):
+                self._bound_methods[obj] = LockMemoFunc(
+                        func,
+                        clear_on_unlock=self._clear_on_unlock,
+                        **kwargs)
+            else:
+                self._bound_methods[obj] = MemoFunc(func, **kwargs)
         return self._bound_methods[obj]
 
     def clear_cache(self, bound=None):
