142d8cec67c39e2db548bd46c103203888eb9534
Jon-Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-01-10 14:13:06 +0100

    Change license in setup.py to MIT

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
