a4cfb5f8eb91282c290a58281038e049bce07a6a
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-01-10 10:54:15 +0100

    Setting version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.rst') ) as f:
 
 setup(
     name="memoclass",
-    version="0.0",
+    version="0.1.0",
     packages=find_packages('src'),
     package_dir={'':'src'},
     license="Apache License 2.0",
