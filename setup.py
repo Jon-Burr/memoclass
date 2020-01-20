e6f8bdd73248802551e3ef4e60d5826543c970ef
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-01-20 18:02:52 +0100

    Tagging version 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(this_directory, 'README.rst') ) as f:
 
 setup(
     name="memoclass",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages('src'),
     package_dir={'':'src'},
     license="MIT",
