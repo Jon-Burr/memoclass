a8b3d96e4eab2e952749716572bfccaada6df84c
Jon Burr <31273281+Jon-Burr@users.noreply.github.com>
2020-01-22 16:34:00 +0100

    Update supported python versions to suit github CI

--- setup.py
+++ setup.py
@@ -26,8 +26,7 @@ setup(
         "future",
         'funcsigs;python_version<"3.0"'
         ],
-    # python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
@@ -35,7 +34,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
